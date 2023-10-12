c1fdfe41e23bbaa0bb67493ce55413fec0e33761
Rahul Patel <whyorean@gmail.com>
2023-10-12 16:14:41 +0530

    Fix WebSearchHelper & align with SearchHelper

--- lib/src/main/java/com/aurora/gplayapi/helpers/WebSearchHelper.kt
+++ lib/src/main/java/com/aurora/gplayapi/helpers/WebSearchHelper.kt
@@ -1,14 +1,16 @@
 package com.aurora.gplayapi.helpers
 
-import com.aurora.gplayapi.Constants
-import com.aurora.gplayapi.data.builders.WebAppBuilder
+import com.aurora.gplayapi.SearchSuggestEntry
 import com.aurora.gplayapi.data.builders.rpc.RpcBuilder
 import com.aurora.gplayapi.data.builders.rpc.SearchQueryBuilder
-import com.aurora.gplayapi.data.models.App
+import com.aurora.gplayapi.data.builders.rpc.SearchSuggestionQueryBuilder
+import com.aurora.gplayapi.data.models.AuthData
+import com.aurora.gplayapi.data.models.SearchBundle
 import com.aurora.gplayapi.utils.dig
 
-class WebSearchHelper {
-    fun searchResults(query: String): MutableMap<Constants.COLLECTION, List<App>> {
+class WebSearchHelper(authData: AuthData) : SearchHelper(authData) {
+    @Throws(Exception::class)
+    override fun searchResults(query: String, nextPageUrl: String): SearchBundle {
         val searchResponse = WebClient().fetch(
             arrayOf(
                 SearchQueryBuilder.build(query)
@@ -17,36 +19,52 @@ class WebSearchHelper {
             RpcBuilder.wrapResponse(it)
         }
 
-        val searchResults = Constants.COLLECTION.values().associateWith { enumValue ->
-            when (enumValue) {
-                Constants.COLLECTION.EXACT -> searchResponse.dig<Collection<Any>>(
-                    SearchQueryBuilder.TAG,
-                    query,
-                    *enumValue.value
-                )
+        val packageNames = searchResponse.dig<Collection<Any>>(
+            SearchQueryBuilder.TAG,
+            query,
+            0,
+            1,
+            0,
+            0,
+            0
+        ).map { it.dig<String>(12, 0) }
 
-                Constants.COLLECTION.QUERY -> searchResponse.dig<Collection<Any>>(
-                    SearchQueryBuilder.TAG,
-                    query,
-                    *enumValue.value
-                )
+        // TODO:: Figure out how to use this nextPageURL
+        val nextPageUrl = searchResponse.dig<String>(
+            SearchQueryBuilder.TAG,
+            query,
+            1,
+            0
+        )
 
-                Constants.COLLECTION.SIMILAR -> searchResponse.dig<Collection<Any>>(
-                    SearchQueryBuilder.TAG,
-                    query,
-                    *enumValue.value
-                ).map { it.dig<Collection<Any>>(0) }
-            }
+        return SearchBundle().apply {
+            this.appList =
+                AppDetailsHelper(authData).getAppByPackageName(packageNames).toMutableList()
+            this.query = query
+            this.subBundles = hashSetOf()
+        }
+    }
+
+    @Throws(Exception::class)
+    override fun searchSuggestions(query: String): List<SearchSuggestEntry> {
+        val searchResponse = WebClient().fetch(
+            arrayOf(
+                SearchSuggestionQueryBuilder.build(query)
+            )
+        ).let {
+            RpcBuilder.wrapResponse(it)
         }
 
-        val apps = searchResults.mapValues { (key, value) ->
-            if (key == Constants.COLLECTION.EXACT) {
-                listOf(WebAppBuilder.buildExactApp(value))
-            } else {
-                value.map { app -> WebAppBuilder.build(app) }
-            }
-        }.toMutableMap()
+        val suggestions = searchResponse.dig<Collection<Any>>(
+            SearchSuggestionQueryBuilder.TAG,
+            query,
+            0
+        ).map {
+            SearchSuggestEntry.newBuilder().apply {
+                title = it.dig(0)
+            }.build()
+        }
 
-        return apps
+        return suggestions
     }
 }
