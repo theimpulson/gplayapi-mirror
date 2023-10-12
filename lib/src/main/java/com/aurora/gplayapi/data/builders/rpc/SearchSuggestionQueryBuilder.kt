5b66f03fb36c846d036c975ad4403f160bca81f6
Rahul Patel <whyorean@gmail.com>
2023-10-12 18:22:58 +0530

    WebSearchHelper: Add support for pagination

--- lib/src/main/java/com/aurora/gplayapi/data/builders/rpc/SearchSuggestionQueryBuilder.kt
+++ lib/src/main/java/com/aurora/gplayapi/data/builders/rpc/SearchSuggestionQueryBuilder.kt
@@ -1,7 +1,7 @@
 package com.aurora.gplayapi.data.builders.rpc
 
 object SearchSuggestionQueryBuilder {
-    public val TAG = javaClass.name
+    public val TAG = "SSQB"
     fun build(query: String, tag: String = TAG): String {
         return """
             ["teXCtc","[null,[\"$query\"],[10],[2,1],4]",null,"$tag@$query"]
