0d3ddc80a49881ce224e3d83c4b3e593710f2bfa
Aayush Gupta <aayushgupta219@gmail.com>
2024-05-30 16:43:55 +0530

    3.2.12
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -68,7 +68,7 @@ publishing {
             create<MavenPublication>("release") {
                 groupId = "com.aurora"
                 artifactId = "gplayapi"
-                version = "3.2.11"
+                version = "3.2.12"
                 from(components["release"])
             }
         }
