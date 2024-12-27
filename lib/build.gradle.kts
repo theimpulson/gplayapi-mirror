30f2b4021402160995164724803005bb199d8918
Aayush Gupta <aayushgupta219@gmail.com>
2024-12-27 14:03:26 +0700

    3.4.5
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -20,7 +20,7 @@ val shouldSignRelease: Boolean
     get() = !tokenUsername.isNullOrEmpty() && !tokenPassword.isNullOrEmpty()
 
 // Bump this version when making a new release
-val libVersion = "3.4.4"
+val libVersion = "3.4.5"
 
 plugins {
     alias(libs.plugins.android.library)
