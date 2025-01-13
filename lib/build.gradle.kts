558a0e105add566a7e5ece25e0b007ccf5553629
Aayush Gupta <aayushgupta219@gmail.com>
2025-01-13 13:33:02 +0700

    gradle: Enable java language 21 features
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -32,6 +32,10 @@ plugins {
     signing
 }
 
+kotlin {
+    jvmToolchain(21)
+}
+
 android {
     namespace = "com.aurora.gplayapi"
     compileSdk = 35
@@ -51,11 +55,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = JavaVersion.VERSION_21.toString()
     }
     packaging {
         resources {
