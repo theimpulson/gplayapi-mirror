1b36d47c3464dfc7e6851a7c497bc1e02b87da44
Aayush Gupta <aayushgupta219@gmail.com>
2025-05-06 15:58:34 +0800

    gplayapi: Override missing equals and hashcode methods for PlayResponse
    Warned by lint
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- lib/src/main/java/com/aurora/gplayapi/data/models/PlayResponse.kt
+++ lib/src/main/java/com/aurora/gplayapi/data/models/PlayResponse.kt
@@ -1,15 +1,29 @@
 /*
  * SPDX-FileCopyrightText: 2020-2021 Aurora OSS
- * SPDX-FileCopyrightText: 2023-2024 The Calyx Institute
+ * SPDX-FileCopyrightText: 2023-2025 The Calyx Institute
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 package com.aurora.gplayapi.data.models
 
+import java.util.UUID
+
 data class PlayResponse(
+    val id: String = UUID.randomUUID().toString(),
     val responseBytes: ByteArray = byteArrayOf(),
     val errorBytes: ByteArray = byteArrayOf(),
     val errorString: String = ("No Error"),
     val isSuccessful: Boolean = false,
     val code: Int = 0
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        return when (other) {
+            is PlayResponse -> other.id == id
+            else -> false
+        }
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
