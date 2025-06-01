6fc5c5dda21fa7b71739060d447e38db802c79af
Rahul Patel <whyorean@gmail.com>
2025-06-01 19:12:45 +0530

    3.5.5

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -20,7 +20,7 @@ val shouldSignRelease: Boolean
     get() = !tokenUsername.isNullOrEmpty() && !tokenPassword.isNullOrEmpty()
 
 // Bump this version when making a new release
-val libVersion = "3.5.4"
+val libVersion = "3.5.5"
 
 plugins {
     alias(libs.plugins.android.library)
